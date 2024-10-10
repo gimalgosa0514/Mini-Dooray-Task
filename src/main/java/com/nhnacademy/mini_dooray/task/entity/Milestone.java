fabd5b55c894d948c8150add1ac7b17e019272a1
정보호 <bigho98@naver.com>
2024-10-10 16:56:16 +0900

    Feat:
    add dependency and @Getter

--- src/main/java/com/nhnacademy/mini_dooray/task/entity/Milestone.java
+++ src/main/java/com/nhnacademy/mini_dooray/task/entity/Milestone.java
@@ -2,10 +2,12 @@ package com.nhnacademy.mini_dooray.task.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class Milestone {
     @Id
     private String milestoneId;
--- src/main/java/com/nhnacademy/mini_dooray/task/entity/ProjectMember.java
+++ src/main/java/com/nhnacademy/mini_dooray/task/entity/ProjectMember.java
@@ -2,8 +2,10 @@ package com.nhnacademy.mini_dooray.task.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class ProjectMember {
     @Id
     private String projectMemberId;
