cad99d22ce05359b075b8542010a6917e27eb2d0
BohoJeong <>
2024-10-11 20:10:12 +0900

    Feat: in progress

---
+++ src/main/java/com/nhnacademy/mini_dooray/task/domain/ResponseMessage.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.mini_dooray.task.domain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ResponseMessage {
+
+    private final String message;
+}
