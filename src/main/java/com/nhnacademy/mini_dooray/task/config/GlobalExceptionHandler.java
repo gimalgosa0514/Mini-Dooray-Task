7090484dd89610c3c555e707f7bd28b4eadea911
Gyubin-Han <yangglei2@naver.com>
2024-10-12 21:42:09 +0900

    feat: Add Task not found exception handle

--- src/main/java/com/nhnacademy/mini_dooray/task/config/GlobalExceptionHandler.java
+++ src/main/java/com/nhnacademy/mini_dooray/task/config/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.nhnacademy.mini_dooray.task.config;
 import com.nhnacademy.mini_dooray.task.domain.ResponseMessage;
 import com.nhnacademy.mini_dooray.task.exception.NoProjectFoundByMemberException;
 import com.nhnacademy.mini_dooray.task.exception.NoSuchProjectFoundException;
+import com.nhnacademy.mini_dooray.task.exception.TaskNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,5 +23,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ResponseMessage> handleNoProjectByMemberException(NoProjectFoundByMemberException e) {
         ResponseMessage errorMessage = new ResponseMessage(e.getMessage());
         return ResponseEntity.status(NOT_FOUND).body(errorMessage);
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ResponseMessage> handleTaskNotFoundException(){
+        return ResponseEntity.status(NOT_FOUND).body(new ResponseMessage("task is not found"));
     }
 }
