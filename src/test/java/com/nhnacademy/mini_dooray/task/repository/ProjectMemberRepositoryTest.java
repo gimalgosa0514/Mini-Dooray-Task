3caec3b38d9e823de8a642d27cbfe8379dda665a
BohoJeong <>
2024-10-12 21:51:26 +0900

    Feat: in progress

--- src/test/java/com/nhnacademy/mini_dooray/task/repository/ProjectMemberRepositoryTest.java
+++ src/test/java/com/nhnacademy/mini_dooray/task/repository/ProjectMemberRepositoryTest.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class ProjectMemberRepositoryTest {
 
     @Autowired
