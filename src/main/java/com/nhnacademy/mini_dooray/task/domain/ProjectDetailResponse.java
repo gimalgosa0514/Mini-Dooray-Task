ea8cd0aa801af3b990e6897815d39d7099f21235
BohoJeong <>
2024-10-13 09:33:07 +0900

    Feat: in progress

--- src/main/java/com/nhnacademy/mini_dooray/task/domain/ProjectDetailResponse.java
+++ src/main/java/com/nhnacademy/mini_dooray/task/domain/ProjectDetailResponse.java
@@ -4,9 +4,11 @@ import com.nhnacademy.mini_dooray.task.entity.Member;
 import com.nhnacademy.mini_dooray.task.entity.ProjectStatus;
 import com.nhnacademy.mini_dooray.task.entity.Task;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @AllArgsConstructor
 public class ProjectDetailResponse {
 
